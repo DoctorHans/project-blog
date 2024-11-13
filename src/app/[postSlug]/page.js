d0c920f5d82931a589ee0af8efde15e90a64b5c1
DoctorHans <DoctorHans@users.noreply.github.com>
2024-11-13 22:27:12 +0100

    Exercise 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -8,6 +8,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 import { BLOG_TITLE } from "@/constants";
+import CodeSnippet from "@/components/CodeSnippet";
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -27,7 +28,7 @@ async function BlogPost({ params }) {
     <article className={styles.wrapper}>
       <BlogHero title={title} publishedOn={publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
